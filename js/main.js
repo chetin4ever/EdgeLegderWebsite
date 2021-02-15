e425da648f07e3785964fcd82a50d9325d93aa19
chetin4ever <hetan009@gmail.com>
2021-02-16 00:48:27 +0530

    converting pages for widescreen and mmobile phone display

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ function initMap() {
   })
   const marker = new google.maps.Marker({ position: loc, map })
 }
+//sticky menu background
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9
+  } else {
+    document.querySelector("#navbar").style.opacity = 1
+  }
+})
+
+//scroll animation
+$("#navbar a , .btn").on("click", function (event) {
+  console.log(this.hash)
+  if (this.hash !== "") {
+    event.preventDefault()
+    const hash = this.hash
+    $("html,body").animate(
+      {
+        scrollTop: $(hash).offset().top - 10,
+      },
+      800
+    )
+  }
+})
